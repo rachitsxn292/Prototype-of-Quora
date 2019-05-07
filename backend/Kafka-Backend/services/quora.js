0ab68c34e5ccd2ed985a6fe1977a491031509e34
divishabera <divistar72@gmail.com>
2019-05-07 02:27:38 -0700

    kafka

--- backend/Kafka-Backend/services/quora.js
+++ backend/Kafka-Backend/services/quora.js
@@ -1,13 +1,169 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var crypto = require('crypto');
+const Profile = require('../models/profile');
+var kafka = require('../kafka/client');
+const fs = require('fs');
+const Topicfollower = require('../models/topicfollower');
+const Topic = require('../models/topic');
+const Question = require('../models/question');
+const Answers = require('../models/answers');
+const Comments = require('../models/comments');
+const Votes = require('../models/votes');
+const Bookmarks = require('../models/bookmarks');
+const Notifications = require('../models/notifications');
+const Followers = require('../models/follower');
+var multer = require('multer');
+const path = require("path");
+const Messages = require('../models/messages');
 
+mongoose.connect('mongodb+srv://root:' +
+process.env.MONGO_PASSWORD+ 
+'@cluster0-kgps1.mongodb.net/quora?retryWrites=true',
+{
+    useNewUrlParser: true
+}
+);
+
+const storage = multer.diskStorage({
+    destination: "../frontend/public/uploads/topic",
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 999999999999999999999999 },
+}).single("myImage");
+
+mongoose.set('useCreateIndex', true)
 
 function handle_request(message, callback) {
 
+    var req = message;
 
+    switch (message.type) {
+        case 'test':
+        try {
+            console.log("In the test", message.type);
+            callback(null, { result: "Success" });
+        }
+        catch (err) {
+            console.log(err);
+        }
+        break;
+
+        case 'topics/get':
+        try {
+            Topic.find()
+            .exec()
+            .then(docs => {
+                console.log(docs);
+                fs.appendFile('logs.txt', 'Status 200, Topics Returned  '+Date.now()+'\n', function (err) {
+                    if (err) throw err;
+                    console.log('Updated!');
+                  });
+                  callback(null, docs);
+                })
+            .catch(err => {
+                callback(null, err);
+
+            })
+        }
+        catch (err) {
+            console.log(err);
+        }
+        break;
+
+        case 'topics/post':
+        try {
+            upload(req, res, (err) => {
+                const param = req.body.param;
+                var picName = "";
+                if(req.file == null || req.file.originalname == null || req.file.originalname == "")
+                {
+                    picName = "default.jpg"; //if no pic was uploaded display default
+                }
+                else
+                {
+                    var picName = req.file.originalname;
+                    var filepath = req.file;   
+                    var filepath = filepath.filename;
+                }
+                var data =  { topic: param, picture: picName};
+                var query = { topic: param },
+                    options = { upsert: true, new: true, setDefaultsOnInsert: true };
+        
+                //console.log("Request file ---", JSON.stringify(req.file));  //Here you get file.
+              
+                // //first you wanna check if topic exists, then create else update
+                Topic.findOneAndUpdate(query,    {  $set : data}, options, function (error, result) {
+                        console.log("resiult", error)
+                   console.log("inside");
+                   fs.appendFile('logs.txt', 'Status 200, Topics Created  '+Date.now()+'\n', function (err) {
+                    if (err) throw err;
+                    console.log('Updated!');
+                  });
+                  callback(null, { result: "Success" });
+
+                });
+        
+            }); //upload end
+        }
+        catch (err) {
+            callback(null, { result: err });
+        }
+        break;
+
+        case 'topics/follow':
+        try {
+            const param = req.body.topic;
+            const follower = req.body.follower;
+            var object = new Topicfollower({ _id: new mongoose.Types.ObjectId(), topic: param, follower: follower });
+            object
+                .save()
+                .then(result => {
+                    console.log(result);
+                }).catch(err => console.log(err));
+                fs.appendFile('logs.txt', 'Status 200, Topics Followed  '+Date.now()+'\n', function (err) {
+                    if (err) throw err;
+                    console.log('Updated!');
+                  });
+                  callback(null, { result: "Success" });
+
+        }
+        catch (err) {
+            callback(null, { result: err });
+        }
+        break;
+
+        case 'topics/isfollowed':
+        try {
+            var topic = req.query.topic;
+            var follower = req.query.follower;
+            Topicfollower.find({topic:topic, follower: follower})
+            .exec()
+            .then(docs => {
+        
+                callback(null, docs);
+            })
+            .catch(err => {
+                callback(null, { result: err });
+
+            })
+        }
+        catch (err) {
+            console.log(err);
+        }
+        break;
+
+    }
                 
-    callback(null, { loadbalancer:"Tested" });
+    //callback(null, { loadbalancer:"Tested" });
 
    
 }
--- backend/api/routes/topic.js
+++ backend/api/routes/topic.js
@@ -8,23 +8,16 @@ const Topicfollower = require('../models/topicfollower');
 const fs = require('fs');
 
 router.get('/', (req, res, next) => {
-    Topic.find()
-        .exec()
-        .then(docs => {
-            console.log(docs);
-            fs.appendFile('logs.txt', 'Status 200 - GET, Topics Returned  '+Date.now()+'\n', function (err) {
-                if (err) throw err;
-                console.log('Updated!');
-              });
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            console.log(err);
+    kafka.make_request("quora", {"type":"topics/get",req:req}, function (err, result) {
+        if (err) {       
             res.status(500).json({
                 error: err
             })
-        })
-
+        }
+        else {
+            res.status(200).json(result);
+        }
+    });
 });
 
 
@@ -42,95 +35,55 @@ const upload = multer({
 }).single("myImage");
 
 router.post('/', (req, res, next) => {
+    kafka.make_request("quora", {"type":"topics/post",req:req}, function (err, result) {
 
-    upload(req, res, (err) => {
-        const param = req.body.param;
-        var picName = "";
-        if(req.file == null || req.file.originalname == null || req.file.originalname == "")
-        {
-            picName = "default.jpg"; //if no pic was uploaded display default
-        }
-        else
-        {
-            var picName = req.file.originalname;
-            var filepath = req.file;   
-            var filepath = filepath.filename;
-        }
-        var data =  { topic: param, picture: picName};
-        var query = { topic: param },
-            options = { upsert: true, new: true, setDefaultsOnInsert: true };
-
-        //console.log("Request file ---", JSON.stringify(req.file));  //Here you get file.
-      
-        // //first you wanna check if topic exists, then create else update
-        Topic.findOneAndUpdate(query,    {  $set : data}, options, function (error, result) {
-                console.log("resiult", error)
-           console.log("inside");
-           fs.appendFile('logs.txt', 'Status 200 - POST, Topics Created  '+Date.now()+'\n', function (err) {
-            if (err) throw err;
-            console.log('Updated!');
-          });
-            res.writeHead(200, {
-                'Content-Type': 'text/plain'
-            });
-            res.end("Topic Created");
+    if (err) {       
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
         });
+        res.end("Topic Created");
+    }
+    else {
+        res.status(200).json(result);
+    }
+});
 
-    }); //upload end
 });
 
 
 
-
-//var object = new Topic({ _id: new mongoose.Types.ObjectId(), topic: param });
-// object
-//     .save()
-//     .then(result => {
-//         console.log(result);
-//     }).catch(err => console.log(err));
-
-//     res.writeHead(200, {
-//         'Content-Type': 'text/plain'
-//     });
-//     res.end("Topic Created");
-
-
 //follow a topic
 router.post('/follow', (req, res, next) => {
-    const param = req.body.topic;
-    const follower = req.body.follower;
-    var object = new Topicfollower({ _id: new mongoose.Types.ObjectId(), topic: param, follower: follower });
-    object
-        .save()
-        .then(result => {
-            console.log(result);
-        }).catch(err => console.log(err));
-        fs.appendFile('logs.txt', 'Status 200 - POST/follow, Topics Followed  '+follower+'  '+Date.now()+'\n', function (err) {
-            if (err) throw err;
-            console.log('Updated!');
-          });
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.end("Topic followed");
+    kafka.make_request("quora", {"type":"topics/follow",req:req}, function (err, result) {
+
+        if (err) {       
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end("Topic Created");
+        }
+        else {
+            res.status(200).json(result);
+        }
     });
+    });
+
 
 //check if a topic is followed
     
     router.get('/isfollowed', (req, res, next) => {
-        var topic = req.query.topic;
-        var follower = req.query.follower;
-        Topicfollower.find({topic:topic, follower: follower})
-        .exec()
-        .then(docs => {
-    
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            })
-        })
+        kafka.make_request("quora", {"type":"topics/isfollowed",req:req}, function (err, result) {
+
+            if (err) {       
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end("Topic Created");
+            }
+            else {
+                res.status(200).json(result);
+            }
+        });
         });
 
 module.exports = router;
